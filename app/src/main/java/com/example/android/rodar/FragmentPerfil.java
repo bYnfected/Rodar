758027edc77cfae633db13e26cd246cbcef71039
Mauricio <mauricio.calgaro@gmail.com>
2019-03-11 22:20:42 -0300

    Novos layouts

--- app/src/main/java/com/example/android/rodar/FragmentPerfil.java
+++ app/src/main/java/com/example/android/rodar/FragmentPerfil.java
@@ -13,6 +13,6 @@ public class FragmentPerfil extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login, container, false);
+        return inflater.inflate(R.layout.perfil, container, false);
     }
 }
