f4dac23e7deb43c094af056541edb3cfe6ea0c75
Mauricio <mauricio.calgaro@gmail.com>
2019-03-11 22:56:49 -0300

    Novos layouts

--- app/src/main/java/com/example/android/rodar/FragmentMensagens.java
+++ app/src/main/java/com/example/android/rodar/FragmentMensagens.java
@@ -13,6 +13,6 @@ public class FragmentMensagens extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mensagens, container, false);
+        return inflater.inflate(R.layout.cadastro_evento, container, false);
     }
 }
