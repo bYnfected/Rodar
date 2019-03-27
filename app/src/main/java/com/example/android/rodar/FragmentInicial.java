6f096a593abe216f79f62ac435e530ccafb64aa8
Mauricio <mauricio.calgaro@gmail.com>
2019-03-27 00:37:21 -0300

    Cadastro FB

--- app/src/main/java/com/example/android/rodar/FragmentInicial.java
+++ app/src/main/java/com/example/android/rodar/FragmentInicial.java
@@ -1,5 +1,6 @@
 package com.example.android.rodar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,10 +10,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.android.rodar.models.Usuario;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class FragmentInicial extends Fragment {
 
     private Button cadastra,entra;
+    private LoginButton loginButtonFB;
+    private CallbackManager callbackManager;
 
 
     @Nullable
@@ -25,6 +45,28 @@ public class FragmentInicial extends Fragment {
 
         cadastra.setOnClickListener(cadastraListener);
         entra.setOnClickListener(entraListener);
+
+        loginButtonFB = v.findViewById(R.id.inicial_button_fb);
+        loginButtonFB.setFragment(this);
+        callbackManager = CallbackManager.Factory.create();
+        loginButtonFB.setReadPermissions(Arrays.asList("email","public_profile"));
+
+        loginButtonFB.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
         return v;
     }
@@ -51,4 +93,53 @@ public class FragmentInicial extends Fragment {
             transaction.commit();
         }
     };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null){
+                Toast.makeText(getContext(), "Usuário Deslogado", Toast.LENGTH_LONG).show();
+            }
+            else {
+                loadUserProfile(currentAccessToken);
+            }
+
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    Usuario usuario = new Usuario();
+                    usuario.setNomeCompleto(object.getString("first_name") + " " + object.getString("last_name"));
+                    usuario.setEmail(object.getString("email"));
+                    usuario.setFacebookId(object.getString("id"));
+
+
+
+                    //String image_url = "https://graph.facebook.com/"+id+ "/picture?type=normal";
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+        Bundle paramenters = new Bundle();
+        paramenters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(paramenters);
+        request.executeAsync();
+
+    }
 }
--- app/src/main/java/com/example/android/rodar/FragmentLogin.java
+++ app/src/main/java/com/example/android/rodar/FragmentLogin.java
@@ -1,5 +1,6 @@
 package com.example.android.rodar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,13 +9,94 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class FragmentLogin extends Fragment {
+
+    private LoginButton loginButtonFB;
+    private CallbackManager callbackManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.login, container, false);
+        View v = inflater.inflate(R.layout.pagina_inicial, container, false);
+
+        loginButtonFB = v.findViewById(R.id.inicial_button_fb);
+        loginButtonFB.setFragment(this);
+        callbackManager = CallbackManager.Factory.create();
+        loginButtonFB.setReadPermissions(Arrays.asList("email","public_profile"));
+
+        loginButtonFB.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                    String image_url = "https://graph.facebook.com/"+id+ "/picture?type=normal";
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+        Bundle paramenters = new Bundle();
+        paramenters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(paramenters);
+
     }
 }
